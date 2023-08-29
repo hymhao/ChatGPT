d4889ecb2ba6fb4969fcb8a1b095b31c4ae8928f
hymhao <141479619+hymhao@users.noreply.github.com>
2023-08-29 13:20:37 +0800

    Update settings.tsx

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -245,7 +245,7 @@ function SyncItems() {
   const syncStore = useSyncStore();
   const webdav = syncStore.webDavConfig;
 
-  // not ready: http://www.ggem666.com
+  // not ready: https://github.com/Yidadaa/ChatGPT-Next-Web/issues/920#issuecomment-1609866332
   return null;
 
   return (
